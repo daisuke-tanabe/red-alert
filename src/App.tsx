a6455bd1174e163ce0540e3723342e44f68b5843
daisuke-tanabe <helloworld0840@gmail.com>
2022-09-10 08:58:28 +0900

    chore: UIフレームワークをセットアップする
    mui, emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 type Sum = {
   (x: number, y: number): number;
 };
 
 const sum: Sum = (x, y) => x + y;
-const sum2: Sum = (x, y) => x + y;
 
 export const App = () => {
   return (
     <>
       <div>{sum(12, 4)}</div>
-      <div>{sum2(12, 4)}</div>
+      <Button variant="contained">Hello World</Button>
       <div>Hello World!</div>
     </>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import theme from '../src/theme';
+
+const emotionCache = createCache({
+  key: 'red-alert',
+});
 
 const root = createRoot(document.getElementById('app') as HTMLElement);
 
-root.render(<App />);
+root.render(
+  <CacheProvider value={emotionCache}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </CacheProvider>,
+);
