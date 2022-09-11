05f1cde2b2cdce1bb1455a956aab79bcf8ea7a12
daisuke-tanabe <helloworld0840@gmail.com>
2022-09-11 15:27:09 +0900

    chore: storybookをセットアップする

---
+++ .storybook/preview.tsx
@@ -0,0 +1,21 @@
+import { ThemeProvider } from '@mui/material'
+import { Story } from '@storybook/react'
+import theme from '../src/theme';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+export const decorators = [
+  (Story: Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+]
---
+++ components/atoms/BaseButton.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import BaseButton from './BaseButton';
+
+export default {
+  title: 'atoms/BaseButton',
+  component: BaseButton,
+} as ComponentMeta<typeof BaseButton>;
+
+const Template: ComponentStory<typeof BaseButton> = (args) => <BaseButton />;
+
+export const FirstStory = Template.bind({});
+
+FirstStory.args = {};
