6562c6b1fe4fd96dfe1fc98ed30248611cb4dedb
daisuke-tanabe <helloworld0840@gmail.com>
2022-09-16 01:34:06 +0900

    change: サイドバーを取り除く

--- src/components/templates/BasetLayout/BaseLayout.tsx
+++ src/components/templates/BasetLayout/BaseLayout.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Box } from '@mui/material';
 import styled from '@emotion/styled';
 import Header from '../../organisms/Header/Header';
-import Sidebar from '../../organisms/Sidebar/Sidebar';
 
 const Main = styled.div`
   display: flex;
@@ -15,12 +14,9 @@ const BaseLayout = (props: BaseLayoutProps) => {
   const { children } = props;
 
   return (
-    <Box css={{ display: 'flex' }}>
-      <Sidebar />
-      <Box css={{ flexGrow: 1 }}>
-        <Header />
-        <Main>{children}</Main>
-      </Box>
+    <Box>
+      <Header />
+      <Main>{children}</Main>
     </Box>
   );
 };
